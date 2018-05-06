1aa758f05bcdd9180d3081795b557adf26e18e31
Monica Marziano <mmarziano1@gmail.com>
2018-05-06 22:11:44 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,17 @@
 class Dog 
+  attr_accessor :name 
   
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all.each do |name|
+      puts Dog.name 
+    end
+  end
   
 end 
